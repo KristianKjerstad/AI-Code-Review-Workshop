3b89b5263faea98f44785883b9122218b4efbb8f
Margrethe By <margrethe.by@hotmail.com>
2025-05-13 18:37:34 +0200

    Color on submit button

--- frontend/src/components/form/SubmitButton.tsx
+++ frontend/src/components/form/SubmitButton.tsx
@@ -8,7 +8,7 @@ export function SubmitButton({handleSubmit, loading}: SubmitButtonProps) {
     return (
         <button
             onClick={handleSubmit}
-            className="px-6 py-2 bg-blue-600 text-white font-semibold rounded shadow hover:bg-blue-700 transition-colors"
+            className="px-6 py-2 bg-[#467865] text-white font-semibold rounded shadow hover:bg-[#2b4a3e] hover:cursor-pointer transition-colors w-full"
             disabled={loading}
         >
             {loading ? 'Reviewing...' : 'Submit code'}
