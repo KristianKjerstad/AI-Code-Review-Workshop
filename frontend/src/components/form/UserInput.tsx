96b19790ea55fe1487ef6cdcc9769a69167d6a3d
Margrethe By <margrethe.by@hotmail.com>
2025-05-13 18:16:07 +0200

    Create component for user input

--- frontend/src/components/form/UserInput.tsx
+++ frontend/src/components/form/UserInput.tsx
@@ -1,11 +1,7 @@
 export function UserInput() {
     return (
-        <div>
-            <label htmlFor="user-input">Code input</label>
-            <textarea name="user-input" id="user-input" placeholder="Write your code">
-
-            </textarea>
+        <div className="flex flex-col my-2">
+            <textarea name="user-input" id="user-input" placeholder="Write your code" className="p-1 h-[200px] w-max-[700px] border-1 rounded-md" />
         </div>
-
     )
 }
